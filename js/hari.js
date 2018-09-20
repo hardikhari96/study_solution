7b24f596f642c52654d02f48b43dc63d7e7834ec
hardikhari96 <harikrushnappatel@gmail.com>
2018-09-21 00:23:51 +0530

    12:23 21-09-2018

--- js/hari.js
+++
@@ -1,5 +0,0 @@
-
-var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || 
-{widgetcode:"27c86ccdf882b856263e87c3c58df4278e5c8651efc3bdbc802aece08b8d1dd35d5fe629768af6d9a03793700d9418c2", values:{},ready:function(){}};
-var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;
-s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
