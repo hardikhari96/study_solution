27f62f72d7ff3d438b8bc8b4bbc8e82ff5618bc6
hardikhari96 <harikrushnappatel@gmail.com>
2018-10-11 21:17:28 +0530

    123

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var user_name = document.getElementById('name').value;
+var email_id = document.getElementById('email').value;
+var phone_no = document.getElementById('phone').value;
+var user_sub = document.getElementById('subject').value;
+var user_msg = document.getElementById('message').value;
+
+
+Function save_user(){
+    
+    
+};
+
+
